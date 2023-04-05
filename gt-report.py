947713f5584e21a84a08efa97e56f7039910cc2c
santiagofacchini <santiago.facchini@icloud.com>
2023-04-05 09:39:57 -0300

    Modified timezone and number of files logic

--- gt-report.py
+++ gt-report.py
@@ -16,21 +16,21 @@ ftp_connection.cwd('/descargas-rosario/gt')
 available_files = ftp_connection.nlst()
 
 if len(available_files) >= 1:
-    print(f'Archivos en {ftp_user} /descargas-rosario/gt para ser cargados en la fuente 11510:\n')
-
     files = []
 
     for file, facts in ftp_connection.mlsd():
         if not os.path.basename(file).startswith('.'):
-            year = facts["modify"][:4]
-            month = facts["modify"][4:6]
-            day = facts["modify"][6:8]
-            hour = facts["modify"][8:]
+            year = facts['modify'][:4]
+            month = facts['modify'][4:6]
+            day = facts['modify'][6:8]
+            hour = int(facts['modify'][9:10]) - 3 # Set local timezone -> 3hs difference
+            minute = facts['modify'][10:12]
 
-            output_format = f'{file} - {day}/{month}/{year} {hour[:2]}:{hour[2:4]}'
+            output_format = f'{file} - {day}/{month}/{year} 0{hour}:{minute}'
 
             files.append(output_format)
 
+    print(f'Archivos en {ftp_user} /descargas-rosario/gt para ser cargados en la fuente 11510: {len(files)}\n')
     [print(x) for x in sorted(files, reverse=True)]
 
 else:
